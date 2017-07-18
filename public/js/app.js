94ae10b889921789bfd6c7ed3d067b0ba0548dfe
sbrycebarker <sbrycesti@gmail.com>
2017-07-18 09:16:57 -0600

    moved index

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@ angular.module("myApp", ['ui.router']).config(function($urlRouterProvider, $stat
           $stateProvider
           .state('home', {
             url: '/',
-            templateUrl: "../views/home.html",
+            templateUrl: "../public/views/home.html",
             controller: 'ctrl'
           })
           // .state('about', {
