fc11de5faafba194752d789eb1a94a201b7c7c12
sbrycebarker <sbrycesti@gmail.com>
2017-07-18 09:26:40 -0600

    works?

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ angular.module("myApp", ['ui.router']).config(function($urlRouterProvider, $stat
           $stateProvider
           .state('home', {
             url: '/',
-            templateUrl: "../public/views/home.html",
+            templateUrl: "../views/home.html",
             controller: 'ctrl'
           })
           // .state('about', {
