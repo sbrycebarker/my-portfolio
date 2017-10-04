73a3d491eb69a8804cd88bcaf1bc75beb7fe7161
sbrycebarker <sbrycesti@gmail.com>
2017-10-04 03:04:49 -0600

    proj

--- js/parallax.js
+++ js/parallax.js
@@ -1,16 +1,15 @@
 $(window).scroll(function(){
  var wScroll = $(this).scrollTop();
- console.log(wScroll)
  $('#top').css({
    'transform' : 'translate(0px, '+ wScroll /100 +'%)'
  })
- var scroll = $(this).scrollTop()
- if ( scroll >= 1200) {
-   scroll = scroll - 1199;
-   console.log("here", scroll)
-   $('.projcon').css({
-     'transform' : 'translate(0px, '+ scroll/5 +'%)'
-   })
- }
+ // var scroll = $(this).scrollTop()
+ // if ( scroll >= 1200) {
+ //   scroll = scroll - 1199;
+ //   console.log("here", scroll)
+ //   $('.projcon').css({
+ //     'transform' : 'translate(0px, '+ scroll/5 +'%)'
+ //   })
+ // }
 
 });
