a24ad76ac7ea2513ad77a739267c9c3ba4652413
sbrycebarker <sbrycesti@gmail.com>
2018-03-08 22:19:33 -0700

    ...

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,15 +1,16 @@
   $('.projcon').css('display', 'none')
 $(document).ready(function(){
+  // $(window).reload()
   // Add scrollspy to <body>
-  function slick() {
-    $('.multiple-items').slick({
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      dots: true
-    });
-  }
-  slick()
+  // function slick() {
+  //   $('.multiple-items').slick({
+  //     infinite: true,
+  //     slidesToShow: 1,
+  //     slidesToScroll: 1,
+  //     dots: true
+  //   });
+  // }
+  // slick()
 
 
   $('.projcon').css('display', 'flex')
