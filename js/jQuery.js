0da1db358d86a58cc358cbfdbdb39a5dd127af4b
sbrycebarker <sbrycesti@gmail.com>
2018-04-15 13:39:43 -0600

    changes

--- js/jQuery.js
+++ js/jQuery.js
@@ -29,7 +29,7 @@ $(document).ready(function(){
       $('html, body').animate({
         scrollTop: $(hash).offset().top - 50
       }, 800, function(){
-        // Add hash (#) to URL when done scrolling (default click behavior)
+
         window.location.hash;
       });
     }  // End if
