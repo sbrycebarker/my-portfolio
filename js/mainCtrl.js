576772a06ea038f6a38785127b683dfddb036a29
sbrycebarker <sbrycesti@gmail.com>
2018-05-26 23:10:27 -0600

    autoplay on

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,6 +1,7 @@
 angular.module('myApp').controller('mainCtrl', function($scope){
   $scope.slick = function() {
     $('.multiple-items').slick({
+      autoplay: true,
       infinite: true,
       slidesToShow: 1,
       slidesToScroll: 1,
